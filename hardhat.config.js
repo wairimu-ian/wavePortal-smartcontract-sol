cc3ed467a64e8dd31fee77cf09ad8ea041f3d48f
ian wairimu <wairimuianmoon@gmail.com>
2022-06-13 17:26:57 +0300

    final commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,13 +19,18 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {
+    },
   rinkeby: {
     url: process.env.STAGING_ALCHEMY_KEY,
-    accounts: [process.env.PRIVATE_KEY],
+    accounts: [process.env.PRIVATE_KEY]
   },
   mainnet: {
     chainId: 1,
     url: process.env.PROD_ALCHEMY_KEY,
     accounts: [process.env.PRIVATE_KEY],
-  }
+ },
+}
 };
